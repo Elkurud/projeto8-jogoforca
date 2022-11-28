66358135bdea84e0caf1f17961de5359ab2741e7
Elkurud <samukama18@gmail.com>
2022-11-28 09:15:53 -0300

    inicio do projeto

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import Jogo from "./Jogo"
+import Letras from "./Letras"
+import Chute from "./Chute.js"
+
+export default function App() {
+    return (
+        <div class="root">
+
+            <span>bom dia</span>
+            <Jogo/>
+            <Letras/>
+            <Chute/>
+            
+        </div>
+    )
+}
